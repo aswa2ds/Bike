eb2d576de627f6b25a25210ab78fa1bb9eda71ee
edwardmartin927 <edwardmartin927@gmail.com>
2018-10-27 15:08:28 +0800

    create class BikeSystem

---
+++ BikeSystem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Edward Martin on 2018/10/27.
+//
+
+#include "BikeSystem.h"
---
+++ BikeSystem.h
@@ -0,0 +1,14 @@
+//
+// Created by Edward Martin on 2018/10/27.
+//
+
+#ifndef BIKE_BIKESYSTEM_H
+#define BIKE_BIKESYSTEM_H
+
+
+class BikeSystem {
+
+};
+
+
+#endif //BIKE_BIKESYSTEM_H
